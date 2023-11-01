a80ede181a79674bd4311c2c3cc8fd1c7f0b136d
Elena Sorokina <chikenjam11@gmail.com>
2023-11-02 00:54:40 +0300

    Initial commit

---
+++ src/main/java/Homework2/AddHomeworks/Third/Cat.java
@@ -0,0 +1,16 @@
+package Homework2.AddHomeworks.Third;
+
+public class Cat {
+    private String name;
+    public Cat(String name) {
+        this.name = name;
+    }
+    public Cat() {
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String begemotik) {
+        this.name = name;
+    }
+}
