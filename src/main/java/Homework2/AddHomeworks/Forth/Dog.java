8f72aa7b179468a1acf827ea97c18992a074eb64
Elena Sorokina <chikenjam11@gmail.com>
2023-11-02 00:56:41 +0300

    Initial commit

--- src/main/java/Homework2/AddHomeworks/Forth/Dog.java
+++ src/main/java/Homework2/AddHomeworks/Forth/Dog.java
@@ -5,7 +5,6 @@ public class Dog implements Runnable {
     public void run() {
         System.out.println("Собака бежит");
     }
-
     public static void main(String[] args) {
         Dog dog = new Dog();
         dog.run();
